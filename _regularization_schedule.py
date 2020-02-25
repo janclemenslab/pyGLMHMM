a68b73a61ddaccdd58ffadeb9cc5f62f5c427971
aslansd <45756533+aslansd@users.noreply.github.com>
2020-02-25 18:39:00 +0100

    Add files via upload

---
+++ _regularization_schedule.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 10:41:28 2020
+
+@author: satarydizaji
+"""
+
+import numpy as np
+
+def _regularization_schedule(iter):
+    tau = 1.5
+
+    # Schedule 1:by iteration
+    if iter < 5:
+        this_lambda = 1
+    else:
+        this_lambda = np.exp((-1) * (iter - 4) / tau) + 0.005
+    
+    return this_lambda
